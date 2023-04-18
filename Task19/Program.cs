b68aa0fe11c4d195d7badd3b1bacf1dcd2980162
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-04-18 23:44:33 +0200

    Start1

---
+++ Task19/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 19: Напишите программу, которая принимает на
+//вход пятизначное число и проверяет, является ли оно
+//палиндромом.
+
+
+int dig = 4;
+int num;
+
+Console.WriteLine("Please enter five-digit number ");
+int.TryParse(Console.ReadLine(), out num);
+
+//CheckText(num);
+//int numcheck = num;
+
+
+
+
+
+
+
+
+//CheckText(num);
+
+//CheckDig(num);
+
+
+
+
+
+void InputNum(int num)
+{
+    int.TryParse(Console.ReadLine(), out num);
+}
+
+
+
+
+
+
+
+void CheckText(int num)
+{
+        while (!int.TryParse(Console.ReadLine(), out num))
+    {
+    Console.WriteLine("This is not number! Please enter fiwe-digit number");
+    }
+}  
+  
+void CheckDig(int num)   
+{
+    while ((int) Math.Log10(num) != dig)
+    {
+    Console.WriteLine("This is not fiwe-digit number! Please enter fiwe-digit number");
+    InputNum(num);
+    }
+}
---
+++ Task21/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+
+
+int dig = 4;
+int num;
+
+
+while (!int.TryParse(Console.ReadLine(), out num))
+{
+    Console.WriteLine("This is not number! Please enter fiwe-digit number");
+}
+
+
+
+while ((int)Math.Log10(num) != dig)
+{
+    Console.WriteLine("This is not fiwe-digit number! Please enter fiwe-digit number");
+}
