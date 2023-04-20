237269e4e56a5c6f0947f95cbb78b26e5978914c
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-04-20 09:21:44 +0200

    Task21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -5,20 +5,49 @@
 //A (3,6,8); B (2,1,-7), -> 15.84
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 
+int x1, x2, y1, y2, z1, z2;
+Console.WriteLine("Please enter the coordinates of the first point (X Y Z): ");
+
+ChekX1Y1Z1();
+
+Console.WriteLine("Please enter the coordinates of the second point (X Y Z): ");
+
+ChekX2Y2Z2();
+
+int x = x2-x1; int y = y2 - y1; int z = z2 - z1;
+double sqrt = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+double lgth =  Math.Round(sqrt, 2); // округление до 2-х знаков после запятой
+Console.WriteLine($"Distance between points: {lgth}");
 
 
-int dig = 4;
-int num;
-
-
-while (!int.TryParse(Console.ReadLine(), out num))
+void ChekX1Y1Z1()
 {
-    Console.WriteLine("This is not number! Please enter fiwe-digit number");
+    while (!int.TryParse(Console.ReadLine(), out x1))
+{
+        Console.WriteLine("This is not number! Please enter number");
+}
+    while (!int.TryParse(Console.ReadLine(), out y1))
+{
+        Console.WriteLine("This is not number! Please enter number");
+}
+    while (!int.TryParse(Console.ReadLine(), out z1))
+{
+        Console.WriteLine("This is not number! Please enter number");
+}
 }
 
-
-
-while ((int)Math.Log10(num) != dig)
+void ChekX2Y2Z2()
 {
-    Console.WriteLine("This is not fiwe-digit number! Please enter fiwe-digit number");
+    while (!int.TryParse(Console.ReadLine(), out x2))
+{
+        Console.WriteLine("This is not number! Please enter number");
+}
+    while (!int.TryParse(Console.ReadLine(), out y2))
+{
+        Console.WriteLine("This is not number! Please enter number");
+}
+    while (!int.TryParse(Console.ReadLine(), out z2))
+{
+        Console.WriteLine("This is not number! Please enter number");
+}
 }
